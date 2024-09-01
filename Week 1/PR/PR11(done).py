fcdfc2f679d2454461c00360a792d540eed0397e
sultanhamdi <sultanhamdi100806@gmail.com>
2024-09-01 10:29:31 +0700

    first commit

--- Week 1/PR/PR11(done).py
+++ Week 1/PR/PR11(done).py
@@ -2,11 +2,14 @@
 m = int(input("Masukkan nilai m (bilangan bulat positif): "))
 n = int(input("Masukkan nilai n (bilangan bulat positif): "))
 
+if (m>n):
 #rumus
-side1 = m**2 - n**2
-side2 = 2 * m * n
-hypotenuse = m**2 + n**2
+    side1 = m**2 - n**2
+    side2 = 2 * m * n
+    hypotenuse = m**2 + n**2
 
-print("Side1 =", side1)
-print("Side2 =", side2)
-print("Hipotenusa=", hypotenuse)
+    print("Side1 =", side1)
+    print("Side2 =", side2)
+    print("Hipotenusa=", hypotenuse)
+else :
+    print("m harus lebih besar dari n")
