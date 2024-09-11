adf5c0924dfdbfe4a875d648c4220ba0775b9b17
sultanhamdi <sultanhamdi100806@gmail.com>
2024-09-11 13:45:37 +0700

    week2

--- Week2/PR/sample Labs IF/LSM2.py
+++ Week2/PR/sample Labs IF/LSM2.py
@@ -1,13 +1,13 @@
-# Input data
-N, C = map(int, input().split())
-# N adalah banyaknya bola
-# C menentukan urutan main, C = 1 (Lala) C = 2 (Lili)
 
-# Cek siapa yang akan menang
-if N % 8 == 0:
+#input
+N, C = map(int, input("Banyak bola dan siapa yang mulai (angka 1 atau 2)=>").split())
+#N adalah jumlah bola
+#C adalah penentu diapa yang memulai, 1 untuk Lala, 2 untuk Lili
+
+# Jika N mod 7 == 0, maka posisi kalah bagi pemain yang memulai
+if N % 7 == 0:
     winner = "Lili" if C == 1 else "Lala"
 else:
     winner = "Lala" if C == 1 else "Lili"
 
-# Output pemenang
 print(winner)
---
+++ tes.py
@@ -0,0 +1,12 @@
+N, C = map(int, input("Banyak bola dan siapa yang mulai (angka 1 atau 2)=>").split())
+#N adalah jumlah bola
+#C adalah penentu diapa yang memulai, 1 untuk Lala, 2 untuk Lili
+
+# Jika N mod 7 == 0, maka posisi kalah bagi pemain yang memulai
+
+if N % 7 == 0:
+    winner = "Lili" if C == 1 else "Lala"
+else:
+    winner = "Lala" if C == 1 else "Lili"
+
+print(winner)
