2b5f512a1d4240af56683102f8c60cdbe058f41f
sultanhamdi <sultanhamdi100806@gmail.com>
2024-09-21 12:35:39 +0700

    .

--- Week3/PR/num7.py.py
+++ Week3/PR/num7.py.py
@@ -1,7 +1,9 @@
 # Logika utama untuk enkripsi atau dekripsi
 mode, key = map(int, input().split())
-pesan = input().lower()  # Memasukkan pesan
+pesan = input()  # Memasukkan pesan
 alphabet = "abcdefghijklmnopqrstuvwxyz"  # Alfabet untuk penggeseran
+alphabet_upper = alphabet.upper()  # Alfabet untuk penggeseran
+
 
 def Enkripsi(pesan, key, alphabet):
     shifted = alphabet[key % 26:] + alphabet[:key % 26]  # Membuat alfabet yang digeser
@@ -17,7 +19,13 @@ def Dekripsi(pesan, key, alphabet):
     shifted = alphabet[-key % 26:] + alphabet[:-key % 26]  # Geser balik untuk dekripsi
     decrypted = ""  # Menyimpan pesan yang telah didekripsi
     for huruf in pesan:
-        if huruf in alphabet:
+        # print("F1", huruf)
+        # print("F2", alphabet)
+        if huruf in alphabet.upper():
+            print(huruf)
+            decrypted += shifted[alphabet_upper.index(huruf)]  # Dekripsi dengan menggeser huruf kembali
+        elif huruf in alphabet.lower():
+            print(huruf)
             decrypted += shifted[alphabet.index(huruf)]  # Dekripsi dengan menggeser huruf kembali
         else:
             decrypted += huruf  # Biarkan spasi dan simbol tetap sama
--- praktikum-1/no-3-lingkaran.py
+++ praktikum-1/no-3-lingkaran.py
@@ -1,13 +1,15 @@
-x1, y1 = map(int(input().split()))
-xs, ys = map(int(input().split()))
-xf, yf = map(int(input().split()))
+# Input data
+x1, y1 = map(int, input().split())
+xs, ys = map(int, input().split())
+xf, yf = map(int, input().split())
 
-jarak_total = ((xf - xs) ** 2 + ((yf - ys) ** 2) ** 0,5)
-langkah = int(jarak_total)
-jarak_awal_center = ((xs - x1) ** 2 + ((ys - y1) ** 2) ** 0.5)
-jarak_akhir_center = ((xf - x1) ** 2 + ((yf - y1) ** 2) ** 0.5)
+jarak_total_kuadrat = (xf - xs) ** 2 + (yf - ys) ** 2
 
-if jarak_awal_center <= 0 or jarak_akhir_center <= langkah:
+jarak_awal_center_kuadrat = (xs - x1) ** 2 + (ys - y1) ** 2
+
+jarak_akhir_center_kuadrat = (xf - x1) ** 2 + (yf - y1) ** 2
+
+if jarak_awal_center_kuadrat <= 0 or jarak_akhir_center_kuadrat <= jarak_total_kuadrat:
     print("No")
 else:
     print("Yes")
